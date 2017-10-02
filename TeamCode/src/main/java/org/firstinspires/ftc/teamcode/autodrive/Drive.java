5eeaeba5a8f15428f5be3f691b3b31c2a4bda2a6
Max Bowman <mbowman@bsd220.org>
2017-10-02 10:35:18 -0500

    First commit.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autodrive/Drive.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.autodrive;
+
+/**
+ * Created by mbowman on 10/2/17.
+ */
+
+public interface Drive {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manual/MecanumManualDrive.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.manual;
+
+/**
+ * Created by mbowman on 10/2/17.
+ */
+
+public class MecanumManualDrive {
+}
